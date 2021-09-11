4bb429ddebf54705e1290625b60edb03a995f1de
Vyrago <52431894+Vyrago@users.noreply.github.com>
2021-09-10 21:12:33 -0700

    Update main.java

--- main/java/panowicz/katherine/main.java
+++ main/java/panowicz/katherine/main.java
@@ -1,1 +1,14 @@
+package panowicz.katherine;
+import animal.*;
+import animal.carnivore.*;
+import animal.herbivore.*;
 
+class Main {
+    public static void main (String args[]) {
+        Lion lion = new Lion();
+        Giraffe giraffe = new Giraffe();
+        
+        System.out.println(lion.speak());
+        System.out.println(giraffe.speak());
+    }
+}
