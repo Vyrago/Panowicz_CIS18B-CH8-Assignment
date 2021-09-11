95b17cd179d501f577e507219609bb8d42505752
Vyrago <52431894+Vyrago@users.noreply.github.com>
2021-09-10 21:18:05 -0700

    Update Giraffe.java

--- main/java/animal/herbivore/Giraffe.java
+++ main/java/animal/herbivore/Giraffe.java
@@ -2,5 +2,6 @@ package animal.herbivore;
 import animal.*;
 
 public class Giraffe implements Animal {
-    
+        // implementation not created, throws an error message warning that no implementation was created.
+
 }
